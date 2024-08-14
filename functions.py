6f5284369022032d67431c622de86f2bc1e26973
Gera-D <daniellsgera@gmail.com>
2024-08-13 21:58:35 -0300

    add sql requirement 1 logic

--- functions.py
+++ functions.py
@@ -88,3 +88,25 @@ def upload_to_bq(df,table_id,schema):
         ,destination=table
         ,job_config=job_config)
     load_job.result()
+
+def hires_per_quarter_2021():
+    query = """SELECT 
+        d.department
+        ,j.job
+        ,COUNTIF(EXTRACT(QUARTER FROM cast(e.datetime as timestamp))=1) as Q1
+        ,COUNTIF(EXTRACT(QUARTER FROM cast(e.datetime as timestamp))=2) as Q2
+        ,COUNTIF(EXTRACT(QUARTER FROM cast(e.datetime as timestamp))=3) as Q3
+        ,COUNTIF(EXTRACT(QUARTER FROM cast(e.datetime as timestamp))=4) as Q4
+        FROM `globant.employees` e
+        INNER JOIN `globant.jobs` j
+        ON e.job_id = j.id
+        INNER JOIN `globant.departments` d
+        ON e.department_id = d.id
+        WHERE EXTRACT(YEAR FROM cast(e.datetime as timestamp))=2021
+        GROUP BY department, job
+        ORDER BY department, job;"""
+    query_job = bigquery_client.query(query=query)
+    response = query_job.result()
+    records = [dict(row) for row in response]
+    response_json = json.dumps(str(records))
+    return response_json
